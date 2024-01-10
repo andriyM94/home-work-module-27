104f3b290b4f8a753482f11e9999c264a67a6f6e
Andriy <andriy.m.94@gmail.com>
2024-01-10 15:40:05 +0200

    home work for module 27

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Створіть функцію-конструктор Calculator, який створює об’єкти з трьома методами:
+// read() запитує два значення за допомогою prompt і запам’ятовує їх у
+// властивостях об’єкта.
+// sum() повертає суму цих властивостей.
+// mul() повертає результат множення даних властивостей
+
+let сalculator = {
+  read() {
+    this.firstNumber = Number.parseFloat(prompt('Введіть перше число'));
+    this.secondNumber = Number.parseFloat(prompt('Введіть друге число'));
+  },
+
+  sum() {
+    return this.firstNumber + this.secondNumber;
+  },
+
+  mul() {
+    return this.firstNumber * this.secondNumber;
+  }
+}
+
+сalculator.read();
+
+console.log(`First number - ${сalculator.firstNumber}`);
+console.log(`Second number - ${сalculator.secondNumber}`);
+
+console.log(`sum() = ${сalculator.sum()}`); 
+alert('sum() - ' + сalculator.sum());
+
+console.log(`mul() = ${сalculator.mul()}`);
+alert('mul() - ' + сalculator.mul());
